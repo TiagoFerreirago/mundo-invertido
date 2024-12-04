e3701512134d9b208670ebb1946c28fa28f46cf1
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-12-04 15:08:06 -0300

    Implementando lógica de inversão do button

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,10 @@
+
+const buttomTheme = document.getElementById("switch-theme-button");
+
+
+buttomTheme.addEventListener("click",() =>{
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
+})
+ 
+    
